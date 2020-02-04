77a3dbeb39a488e5392b9068760a027f1a908a62
Steve Jones <steve@iaascream.cloud>
2020-02-03 22:59:31 -0800

    RDS service is tech preview

--- clc/modules/rds-common/src/main/java/com/eucalyptus/rds/common/Rds.java
+++ clc/modules/rds-common/src/main/java/com/eucalyptus/rds/common/Rds.java
@@ -13,6 +13,7 @@ import com.eucalyptus.component.annotation.AwsServiceName;
 import com.eucalyptus.component.annotation.Description;
 import com.eucalyptus.component.annotation.Partition;
 import com.eucalyptus.component.annotation.PublicService;
+import com.eucalyptus.util.techpreview.TechPreview;
 
 /**
  *
@@ -22,6 +23,7 @@ import com.eucalyptus.component.annotation.PublicService;
 @PolicyVendor("rds")
 @Partition(value = Rds.class, manyToOne = true)
 @Description("Amazon RDS API service")
+@TechPreview(enableByDefaultProperty = "enable.rds.tech.preview")
 public class Rds extends ComponentId {
 
   private static final long serialVersionUID = 1L;
